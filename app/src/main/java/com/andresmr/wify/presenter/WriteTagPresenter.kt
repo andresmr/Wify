b04d40e8961e3f567f7d18d45f94dec4e003dfbd
andresmr <personal@andresmr.com>
2018-12-13 20:29:43 +0100

    Add vibration to write in tag

--- app/src/main/java/com/andresmr/wify/presenter/WriteTagPresenter.kt
+++ app/src/main/java/com/andresmr/wify/presenter/WriteTagPresenter.kt
@@ -4,53 +4,56 @@ import com.andresmr.wify.domain.CreateNetInteractor
 import com.andresmr.wify.entity.Net
 
 class WriteTagPresenter(private val view: View,
-    private val createNetInteractor: CreateNetInteractor) {
+                        private val createNetInteractor: CreateNetInteractor) {
 
-  init {
-    view.setListeners()
-  }
-
-  fun onPause() {
-    view.pauseNFCInForeground()
-    view.hideScanInfo()
-  }
-
-  fun onWriteTagClick(ssid: String, password: String) {
-    when {
-      ssid.isEmpty() -> view.showErrorOnSSID()
-      password.isEmpty() -> view.showErrorOnPassword()
-      else -> {
-        view.enableNFCInForeground()
-        view.showScanInfo()
-      }
+    init {
+        view.setListeners()
     }
-  }
 
-  fun onTagDetected(ssid: String, password: String) {
-    val net = createNetInteractor.execute(ssid, password)
-    view.writeNetOnTag(net)
-  }
+    fun onPause() {
+        view.pauseNFCInForeground()
+        view.hideScanInfo()
+    }
 
-  fun onWriteOnTagSuccessful() {
-    view.showWriteOnTagSuccessfulInfo()
-    view.pauseNFCInForeground()
-  }
+    fun onWriteTagClick(ssid: String, password: String) {
+        when {
+            ssid.isEmpty() -> view.showErrorOnSSID()
+            password.isEmpty() -> view.showErrorOnPassword()
+            else -> {
+                view.enableNFCInForeground()
+                view.showScanInfo()
+            }
+        }
+    }
 
-  fun onWriteOnTagError() {
-    view.showWriteOnTagErrorInfo()
-    view.pauseNFCInForeground()
-  }
+    fun onTagDetected(ssid: String, password: String) {
+        val net = createNetInteractor.execute(ssid, password)
+        view.writeNetOnTag(net)
+    }
 
-  interface View {
-    fun setListeners()
-    fun enableNFCInForeground()
-    fun pauseNFCInForeground()
-    fun showScanInfo()
-    fun hideScanInfo()
-    fun showErrorOnSSID()
-    fun showErrorOnPassword()
-    fun writeNetOnTag(net: Net)
-    fun showWriteOnTagSuccessfulInfo()
-    fun showWriteOnTagErrorInfo()
-  }
+    fun onWriteOnTagSuccessful() {
+        view.vibrate()
+        view.showWriteOnTagSuccessfulInfo()
+        view.pauseNFCInForeground()
+    }
+
+    fun onWriteOnTagError() {
+        view.vibrate()
+        view.showWriteOnTagErrorInfo()
+        view.pauseNFCInForeground()
+    }
+
+    interface View {
+        fun setListeners()
+        fun enableNFCInForeground()
+        fun pauseNFCInForeground()
+        fun showScanInfo()
+        fun hideScanInfo()
+        fun showErrorOnSSID()
+        fun showErrorOnPassword()
+        fun writeNetOnTag(net: Net)
+        fun showWriteOnTagSuccessfulInfo()
+        fun showWriteOnTagErrorInfo()
+        fun vibrate()
+    }
 }
--- app/src/main/java/com/andresmr/wify/ui/WriteTagView.kt
+++ app/src/main/java/com/andresmr/wify/ui/WriteTagView.kt
@@ -7,7 +7,10 @@ import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.nfc.tech.Ndef
 import android.nfc.tech.NdefFormatable
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import androidx.appcompat.app.AppCompatActivity
 import com.andresmr.wify.ui.util.NFCUtil
 import com.andresmr.wify.entity.Net
@@ -21,81 +24,90 @@ import kotlinx.android.synthetic.main.write_tag_view.writeTagButton
 
 class WriteTagView : AppCompatActivity(), WriteTagPresenter.View {
 
-  private lateinit var nfcAdapter: NfcAdapter
-  private lateinit var presenter: WriteTagPresenter
-  private lateinit var nfcUtil: NFCUtil
+    private lateinit var nfcAdapter: NfcAdapter
+    private lateinit var presenter: WriteTagPresenter
+    private lateinit var nfcUtil: NFCUtil
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.write_tag_view)
-    nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-    presenter = WriteTagPresenter(this, CreateNetInteractor())
-    nfcUtil = NFCUtil()
-  }
-
-  override fun onPause() {
-    super.onPause()
-    presenter.onPause()
-  }
-
-  override fun onNewIntent(intent: Intent?) {
-    super.onNewIntent(intent)
-    presenter.onTagDetected(ssid.editText!!.text.toString(), password.editText!!.text.toString())
-  }
-
-  override fun setListeners() {
-    writeTagButton.setOnClickListener {
-      presenter.onWriteTagClick(ssid.editText!!.text.toString(),
-          password.editText!!.text.toString())
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.write_tag_view)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        presenter = WriteTagPresenter(this, CreateNetInteractor())
+        nfcUtil = NFCUtil()
     }
-  }
 
-  override fun enableNFCInForeground() {
-    val pendingIntent = PendingIntent.getActivity(this, 0,
-        Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
-    val nfcIntentFilter = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
-    val filters = arrayOf(nfcIntentFilter)
-    val techLists = arrayOf(arrayOf(Ndef::class.java.name),
-        arrayOf(NdefFormatable::class.java.name))
-    nfcAdapter.enableForegroundDispatch(this, pendingIntent, filters, techLists)
-  }
-
-  override fun pauseNFCInForeground() {
-    nfcAdapter.disableForegroundDispatch(this)
-  }
-
-  override fun showScanInfo() {
-    scanningInfo.text = getString(R.string.scan_info)
-  }
-
-  override fun hideScanInfo() {
-    scanningInfo.text = null
-  }
-
-  override fun showErrorOnSSID() {
-    ssid.error = getString(R.string.error_field_null)
-  }
-
-  override fun showErrorOnPassword() {
-    password.error = getString(R.string.error_field_null)
-
-  }
-
-  override fun writeNetOnTag(net: Net) {
-    val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-
-    if (nfcUtil.writeOnTag(net, tag)) {
-      presenter.onWriteOnTagSuccessful()
-    } else {
-      presenter.onWriteOnTagError()
+    override fun onPause() {
+        super.onPause()
+        presenter.onPause()
     }
-  }
 
-  override fun showWriteOnTagSuccessfulInfo() {
-    scanningInfo.text = getString(R.string.write_tag_success)
-  }
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        presenter.onTagDetected(ssid.editText!!.text.toString(), password.editText!!.text.toString())
+    }
 
-  override fun showWriteOnTagErrorInfo() {
-    scanningInfo.text = getString(R.string.write_tag_error)
-  }
+    override fun setListeners() {
+        writeTagButton.setOnClickListener {
+            presenter.onWriteTagClick(ssid.editText!!.text.toString(),
+                    password.editText!!.text.toString())
+        }
+    }
+
+    override fun enableNFCInForeground() {
+        val pendingIntent = PendingIntent.getActivity(this, 0,
+                Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
+        val nfcIntentFilter = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
+        val filters = arrayOf(nfcIntentFilter)
+        val techLists = arrayOf(arrayOf(Ndef::class.java.name),
+                arrayOf(NdefFormatable::class.java.name))
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, filters, techLists)
+    }
+
+    override fun pauseNFCInForeground() {
+        nfcAdapter.disableForegroundDispatch(this)
+    }
+
+    override fun showScanInfo() {
+        scanningInfo.text = getString(R.string.scan_info)
+    }
+
+    override fun hideScanInfo() {
+        scanningInfo.text = null
+    }
+
+    override fun showErrorOnSSID() {
+        ssid.error = getString(R.string.error_field_null)
+    }
+
+    override fun showErrorOnPassword() {
+        password.error = getString(R.string.error_field_null)
+
+    }
+
+    override fun writeNetOnTag(net: Net) {
+        val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+
+        if (nfcUtil.writeOnTag(net, tag)) {
+            presenter.onWriteOnTagSuccessful()
+        } else {
+            presenter.onWriteOnTagError()
+        }
+    }
+
+    override fun showWriteOnTagSuccessfulInfo() {
+        scanningInfo.text = getString(R.string.write_tag_success)
+    }
+
+    override fun showWriteOnTagErrorInfo() {
+        scanningInfo.text = getString(R.string.write_tag_error)
+    }
+
+    override fun vibrate() {
+        val v = getSystemService(VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            v.vibrate(500)
+        }
+    }
 }
